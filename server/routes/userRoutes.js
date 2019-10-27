962d4978244ce116d5cd3782658b713059dfe909
Marion <marion_ott@me.com>
2019-10-27 16:08:04 +0100

    update router init

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('./../controllers/userController')
+const authController = require('./../controllers/authController')
 
 router
     .route('/')
@@ -10,4 +11,8 @@ router
     .route('/:id')
     .get(userController.getUser)
 
+router
+    .route('/signup')
+    .post(authController.signup)
+    
 module.exports = router;
