9153b1fa6f9ab034ced85acb6054183307bae53c
Marion <marion_ott@me.com>
2019-11-03 13:54:29 +0100

    update edit function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,14 +1,40 @@
-//TODO: Verify that a user is connected in order to access students list
-exports.getAllUsers = (req, res) => {
-    return res.status(200).json({
-        status: 'success',
-        data: {}
-    })
-}
+const User = require('./../models/userModel')
+const catchAsync = require('./../services/catchAsync')
 
-exports.getUser = (req, res) => {
-    return res.status(200).json({
+exports.getAllUsers = catchAsync(async (req, res) => {
+    const users = await User.find()
+    res.status(200).json({
         status: 'success',
-        data: {}
+        data: {
+            users
+        }
     })
-}
+})
+
+exports.getUser = catchAsync(async (req, res) => {
+    const user = await User.findById(req.params.id)
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user
+        }
+    })
+})
+
+//TODO: Verify that a user is connected in order to edit data
+exports.updateUser = catchAsync(async (req, res, next) => {
+    const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        req.body, {
+            new: true,
+            runValidators: true
+        }
+    );
+
+    res.status(200).json({
+        status: 'Success',
+        data: {
+            user: updatedUser
+        }
+    });
+})
